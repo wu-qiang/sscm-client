0bfdef4e4523439e0f4dffc73d92399b0d97be25
wmhopkins <will.hopkins@oracle.com>
2018-03-28 17:01:30 -0400

    test build changes

--- scripts/setup-script.sh
+++ scripts/setup-script.sh
@@ -57,9 +57,9 @@ echo "Execute: $SCRIPTS_DIR/policy-test.sh"
 $SCRIPTS_DIR/policy-test.sh
 
 cd ${WERCKER_SOURCE_DIR}
-declare groupId=$(mvn help:evaluate -Dexpression=project.groupId | grep -v "^\[INFO\]")
-declare artifactId=$(mvn help:evaluate -Dexpression=project.artifactId | grep -v "^\[INFO\]")
-declare version=$(mvn help:evaluate -Dexpression=project.version | grep -v "^\[INFO\]")
+declare groupId=$(mvn -Dmaven.repo.local=${WERCKER_CACHE_DIR}/.m2 help:evaluate -Dexpression=project.groupId | grep -v "^\[INFO\]")
+declare artifactId=$(mvn -Dmaven.repo.local=${WERCKER_CACHE_DIR}/.m2 help:evaluate -Dexpression=project.artifactId | grep -v "^\[INFO\]")
+declare version=$(mvn -Dmaven.repo.local=${WERCKER_CACHE_DIR}/.m2 help:evaluate -Dexpression=project.version | grep -v "^\[INFO\]")
 declare timestamp=$(date -u -Ins)
 echo "Generating resourceUrl ..."
 echo "echo '${groupId}:${artifactId}:${version}-${timestamp}' > $RESOURCE_URL_FILE"
