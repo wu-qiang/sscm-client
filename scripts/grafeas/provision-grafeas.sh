4d5ed20655d31f60995d1925edef7f9cbc909769
wmhopkins <will.hopkins@oracle.com>
2018-03-29 22:02:49 -0400

    Provision attestation authorities

--- scripts/grafeas/provision-grafeas.sh
+++ scripts/grafeas/provision-grafeas.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 #
-# Script to provision project names into grafeas server
+# Script to provision project names and attestation
+# authorities into grafeas server
 #
 # host:port for grafeas server passed as $1
 #
 
+declare GRAFEAS_PROJECTS_URL=
+declare GRAFEAS_NOTES_URL=
+
 declare PROJECT_NAMES="projects/weblogic-kubernetes-operator projects/build-infrastructure"
-declare GRAFEAS_URL=
+declare AUTHORITY_NAMES="Build Test SecurityScan"
 
 if [[ -n "$1" ]] ; then
-    GRAFEAS_URL="http://${1}/v1alpha1/projects"
-    echo "GRAFEAS_URL set to '$GRAFEAS_URL'"
+    GRAFEAS_PROJECTS_URL="http://${1}/v1alpha1/projects"
+    echo "GRAFEAS_PROJECTS_URL set to '$GRAFEAS_PROJECTS_URL'"
+    GRAFEAS_NOTES_URL="http://${1}/v1alpha1/projects/build-infrastructure/notes"
+    echo "GRAFEAS_NOTES_URL set to '$GRAFEAS_NOTES_URL'"
 else
     echo "Grafeas host:port not provided!"
     exit 1
 fi
 
-grafeas_project_exists() {
-    local project="$1"
-    if curl -s -X GET "$GRAFEAS_URL" | jq '.projects|.[]|.name' | grep -- "\"${project}\"" > /dev/null ; then
-        return 0
-    fi
-    return 1
+project_exists() {
+    curl -s -X GET "$GRAFEAS_PROJECTS_URL" | jq -e ".projects|.[]|select(.name == \"${1}\")" > /dev/null
 }
 
-grafeas_create_project() {
-    local project="$1"
-    curl -s -X POST "$GRAFEAS_URL" -d "{\"name\":\"${project}\"}" > /dev/null || return 1
-    return 0
+create_project() {
+    curl -s -X POST "$GRAFEAS_PROJECTS_URL" -d "{\"name\":\"${1}\"}" > /dev/null
 }
 
 provision_project_names() {
@@ -36,13 +36,57 @@ provision_project_names() {
     local status=0
     for project in $PROJECT_NAMES
     do
-        if grafeas_project_exists "$project" ; then
+        if project_exists "$project" ; then
             echo "Grafeas project '$project' already exists"
         else
             echo "Creating Grafeas project '$project' ..."
-            grafeas_create_project "$project"
-            if ! grafeas_project_exists "$project" ; then
+            create_project "$project"
+            if [[ $? -ne 0 ]] || ! project_exists "$project" ; then
                 echo "Couldn't create Grafeas project '$project'!"
+                ((status++))
+            fi
+        fi
+    done
+}
+
+authority_exists() {
+    local note_name="projects/build-infrastructure/notes/${1}AttestationAuthority"
+    curl -s -X GET "$GRAFEAS_NOTES_URL" | jq -e ".notes|.[]|select(.name == \"${note_name}\")" > /dev/null
+}
+
+create_authority() {
+    local note_name="projects/build-infrastructure/notes/${1}AttestationAuthority"
+    local short_desc="build-infrastructure:${1}"
+    local long_desc="Oracle Grafeas ${1} Metadata Generator"
+    local authority_name="projects/build-infrastructure/attestationAuthorities/${1}"
+
+    curl -s -X POST -d @- "$GRAFEAS_NOTES_URL" <<EOF > /dev/null
+    {
+        "name": "${note_name}",
+        "shortDescription": "${short_desc}",
+        "longDescription": "${long_desc}",
+        "kind": "ATTESTATION_AUTHORITY",
+        "attestationAuthority": {
+            "hint": {
+                "humanReadableName": "${authority_name}"
+            }
+        }
+    }
+EOF
+}
+
+provision_authorities() {
+    local authority=
+    local status=0
+    for authority in $AUTHORITY_NAMES
+    do
+        if authority_exists "$authority" ; then
+            echo "Attestation authority for '$authority' already exists"
+        else
+            echo "Creating attestation authority for '$authority' ..."
+            create_authority "$authority"
+            if [[ $? -ne 0 ]] || ! authority_exists "$authority" ; then
+                echo "Couldn't create attestation authority for '$authority'!"
                 ((status++))
             fi
         fi
@@ -52,6 +96,7 @@ provision_project_names() {
 declare estatus=0
 
 provision_project_names || ((estatus++))
+provision_authorities || ((estatus++))
 
 exit $estatus
 
